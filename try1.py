107009e06c04d86735da5b3b9f99a9ecf734ad00
anuragkedia <anuragkedia2608@gmail.com>
2020-09-09 00:14:42 +0530

    Protytype cleaned

--- try1.py
+++
@@ -1,8 +0,0 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-s.bind(("127.0.0.2", 8080))
-s.connect(("127.0.0.1", 12345))
-s.send(b'try1')
-s.close()
--- try3.py
+++
@@ -1,8 +0,0 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-s.bind(("127.0.0.4", 8082))
-s.connect(("127.0.0.1", 12345))
-s.send(b'try3')
-s.close()
